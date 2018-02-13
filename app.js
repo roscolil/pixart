1746bf19f25e024bc4566dc4ed51928433fc4fc4
roscolil <ross.lillis@gmail.com>
2018-02-13 14:57:32 +1100

    commit 3: created 100 divs

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ $("input").keypress(function() {
   var $newBrushColor = $('input').val();
   $brushBox.css("background-color", $newBrushColor);
   });
+
+  $(document).ready(function () {
+      var $newdiv;
+      for (var i = 0; i < 100; i++) {
+          $newdiv = $('<div class="square" />');
+          $('body').append($newdiv);
+      }
+  });
