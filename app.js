dd21c1389f0ef7b915aee8dc819de2c19732630c
roscolil <ross.lillis@gmail.com>
2018-02-13 14:50:31 +1100

    brush box changing color on keypress

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+// Event listener for button click event
 $("button").on("click", function() {
   var $brushBox = $(".brush-box");
   var $newBrushColor = $('input').val();
-
   $brushBox.css("background-color", $newBrushColor);
 });
+
+// Event listener for input box keypress event
+$("input").keypress(function() {
+  var $brushBox = $(".brush-box");
+  var $newBrushColor = $('input').val();
+  $brushBox.css("background-color", $newBrushColor);
+  });
